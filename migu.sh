5d42a700b6083e8983c65d8d01460803781c30f2
bean0283 <1197016544@qq.com>
2022-04-19 10:51:51 +0800

    Update migu.sh

--- migu.sh
+++ migu.sh
@@ -1,11 +1,19 @@
-#!/usr/bin/env bash 
-
-echo ''
-echo '---------------check sign---------------'
 curl --location --request POST 'https://cwv587.com/user/checkin' \
 --header 'Host: cwv587.com' \
 --header 'Content-Length: 0' \
---header 'Cookie: PHPSESSID=lt5fds20mc43och6g0ip5fiskq; __cf_bm=BUayHvSG64Oxrvj0V98.g9Lq50Cex3fIG6d40N.3iX0-1647479543-0-AQ4WVISe+3iWusnEN+4N+aOtAcf4CZhzyr3Vyby0/UsFwR2yR9153s8IY/yV3vB7p2JjHhW2m0ZbttwXWvziAhNmCRt8qo8XHaQDulnEdOrZt3GKM/h4x8DBRBgfNGVYiA==; uid=29289; email=1197016544%40qq.com; key=257dc77e888bfd2556a4d6060690eccd12d742a00ec7b; ip=8fac1fc3647be6a974d7f4c45e5be97e; expire_in=1648084212; mtauth=5e2ceba40a7bb123052965a882244271; cf_clearance=e.KSg04MvaYfSGZVJC9XRxdeH3QGuXKQXaVB_JNYO7s-1647479526-0-250; lang=zh-cn' \
+--header 'Cookie: uid=29368; email=969191717%40qq.com; key=ea0bc5932f7af58114bc753b4bc8562f556d29838f605; ip=9f2bedae2bb4f3a901a465f5814b852a; expire_in=1650192118; lang=zh-cn; mtauth=f571ecc2c0d18ef45cc49ed521907cc9; __cf_bm=L3g7qWsLY6KnUyRRl5VlOPLmtZVcmwoXjRKDfJLMgVM-1649896777-0-AVPUQ2UhtKRnhr40Uz7+JbV1AS3kr+CG+Ki2bXFKX6iwUmovL5JQd7Yamp51SfnGWHl2NYa9HdSCpEkx334a4AMysHNxhW1OgWQhwiI9o+28u8GRzEHTFCr8HtNWSGy7Bw==; pop=yes' \
+--header 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0' \
+--header 'Accept: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0 application/json, text/javascript, */*; q=0.01' \
+--header 'Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2' \
+--header 'Accept-Encoding: gzip, deflate, br' \
+--header 'Origin: https://cwv587.com' \
+--header 'Connection: keep-alive' \
+--header 'Content-Type: text/xml'
+
+curl --location --request POST 'https://cwv587.com/user/checkin' \
+--header 'Host: cwv587.com' \
+--header 'Content-Length: 0' \
+--header 'Cookie: ip=8929518f39e7d183a2c0997a80d8028e; expire_in=1650501794; lang=zh-cn; mtauth=5e2ceba40a7bb123052965a882244271; __cf_bm=L3g7qWsLY6KnUyRRl5VlOPLmtZVcmwoXjRKDfJLMgVM-1649896777-0-AVPUQ2UhtKRnhr40Uz7+JbV1AS3kr+CG+Ki2bXFKX6iwUmovL5JQd7Yamp51SfnGWHl2NYa9HdSCpEkx334a4AMysHNxhW1OgWQhwiI9o+28u8GRzEHTFCr8HtNWSGy7Bw==; PHPSESSID=viskfo74el47navkevf9juho4l; uid=29289; email=1197016544%40qq.com; key=44c27231f83c822b7fa64bd2026b71ba7d50a27bd9800; pop=yes' \
 --header 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0' \
 --header 'Accept: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0 application/json, text/javascript, */*; q=0.01' \
 --header 'Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2' \
